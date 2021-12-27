df5dcd96a13d6766f26ab58644dc703b5407223c
luciaondovcik <55868872+luciaondovcik@users.noreply.github.com>
2021-12-27 20:05:31 +0100

    final 1.1 bez casu

--- main.cpp
+++ main.cpp
@@ -6,23 +6,22 @@
 #include <algorithm>
 
 const int SIZE = 8;
-const int MINMAX_DEPTH = 5;
+const int MINMAX_DEPTH = 4;
 
 struct player {
     char color;
     char character;
     int time;
-    int score = 0;
 };
 player my_player;
 
 
 struct move {
-    int start;
-    int end;
-    int score;
-    std::vector<int> flippable_coors;
+    size_t end;
+    std::vector<size_t> flippable_coors;
 };
+
+void find_tile(size_t tile);
 
 
 // ERROR
@@ -57,6 +56,10 @@ void check_number(std::string number) {
     if (!is_digits(number)) {
         throw_error("Invalid number.");
     }
+    int num = stoi(number);
+    if (num < 2 || num > 30) {
+        throw_error("Too large number.");
+    }
 }
 
 
@@ -67,7 +70,7 @@ void check_color(std::string color) {
 }
 
 
-void check_length(std::vector<std::string>& parameters, int length) {
+void check_length(std::vector<std::string>& parameters, size_t length) {
     if (parameters.size() != length) {
         throw_error("Invalid number of parameters.");
     }
@@ -84,7 +87,7 @@ void parse_command(std::string command, std::vector<std::string>& parameters) {
 
 
 // GAME MOVES
-bool out_of_board(int act_spot, int neighbour) {
+bool out_of_board(size_t act_spot, int neighbour) {
     if (((act_spot + neighbour) < 0) || ((act_spot + neighbour) > 63) || (((act_spot % SIZE) == 0) && ((neighbour % SIZE) == SIZE - 1)) || (((act_spot % SIZE) == SIZE - 1) && ((neighbour % SIZE) == 0))) {
         return true;
     }
@@ -99,26 +102,30 @@ bool empty_spot(char act_spot) {
 }
 
 
-void check_neighbours(std::string& act_game, int position, std::vector<move>& possible_moves, char player) {
+void check_neighbours(std::string& act_game, size_t position, std::vector<move>& possible_moves, char player) {
     std::vector<int> neighbours{ -SIZE - 1, -SIZE, -SIZE + 1 , -1, +1, +SIZE - 1, +SIZE , +SIZE + 1 };
-    std::vector<int> flippable_temp;
+    std::vector<size_t> flippable_temp;
     flippable_temp.push_back(position); // add start position
 
-    for (int i = 0;i < neighbours.size();i++) {    
-        int curr_position = position + neighbours[i];
+    for (size_t i = 0;i < neighbours.size();i++) {
+        size_t curr_position = position + neighbours[i];
+        size_t counter = 0;
 
         do {            
             // check out of board
-            if (out_of_board(curr_position, neighbours[i])) {
+            if (out_of_board(curr_position - neighbours[i], neighbours[i])) {
                 break;
             }
             // if next spot is also empty, nothing to flip            
             if (empty_spot(act_game[curr_position])) {
+                // if there is a row with same color and then empty spot -> cannot flip
+                flippable_temp.erase(flippable_temp.end() - counter, flippable_temp.end());
                 break;
             }
             // if opposite color count sum, else break bcs found start point
             if (act_game[curr_position] != player) {
                 flippable_temp.push_back(curr_position);
+                counter++;
             }
             else {
                 break;
@@ -128,11 +135,9 @@ void check_neighbours(std::string& act_game, int position, std::vector<move>& po
     }
 
     // if is at least one flipable spot, add to possible moves
-    if (flippable_temp.size() > 0) {
+    if (flippable_temp.size() > 1) {
         move movement;
-        //movement.start = curr_position;
         movement.end = position;
-        //movement.score = flippable_temp.size();
         movement.flippable_coors = flippable_temp;
         possible_moves.push_back(movement);
     }
@@ -142,7 +147,7 @@ void check_neighbours(std::string& act_game, int position, std::vector<move>& po
 std::vector<move> find_possible_moves(std::string& act_game, char player) {
     std::vector<move> possible_moves;
 
-    for (int i = 0; i < act_game.size();i++) {
+    for (size_t i = 0; i < act_game.size();i++) {
         if (empty_spot(act_game[i])) {
             check_neighbours(act_game, i, possible_moves, player);
         }
@@ -152,47 +157,57 @@ std::vector<move> find_possible_moves(std::string& act_game, char player) {
 }
 
 
-void do_move(std::string& temp_game, move movement) {
-    for (int i = 0;i < movement.flippable_coors.size();i++) {
-        int coor = movement.flippable_coors[i];
-        (my_player.character == 'O') ? (temp_game[coor] = 'O') : (temp_game[coor] = 'X');
+void do_move(std::string& temp_game, move movement, char player) {
+    for (size_t i = 0;i < movement.flippable_coors.size();i++) {
+        size_t coor = movement.flippable_coors[i];
+        (player == 'O') ? (temp_game[coor] = 'O') : (temp_game[coor] = 'X');
     }
+}
+
+
+// NEFUNGUJE GAME OVER IDEM SPAT MAM PICI
+bool game_over(std::string& act_game) {
+    char opp = (my_player.character == 'O') ? ('X') : ('O');
+    std::vector<move> my_moves = find_possible_moves(act_game, my_player.character);
+    std::vector<move> opp_moves = find_possible_moves(act_game, opp);
+
+    return my_moves.size() == 0 && opp_moves.size() == 0;
 }
 
 
 // MINMAX TREE THINGS
 struct minmax_tree {
-    minmax_tree** leaves;
-    int leaves_count;
+    std::vector<minmax_tree> leaves;
+    size_t leaves_count;
     std::string game;
     double value;
-    int tile;
+    size_t tile;
 };
 
 
-minmax_tree *create_tree(int depth, std::string act_game, int act_tile) {
-    minmax_tree *node;
-    std::vector<move> act_move_list = find_possible_moves(act_game, my_player.character);
-    node->leaves_count = act_move_list.size();
-    std::memcpy(&node->game, &act_game, sizeof act_game);
-    node->tile = act_tile;
+minmax_tree create_tree(int depth, std::string act_game, size_t act_tile, char player) {
+    minmax_tree node;
+    std::vector<move> act_move_list = find_possible_moves(act_game, player);
+    node.leaves_count = act_move_list.size();
+    node.game = act_game;
+    node.tile = act_tile;
+
+    if (depth == 3) {
+        find_tile(node.tile);
+    }
 
     // create leaves if we're not too deep and exists at least one move
-    if (depth > 0 && node->leaves_count > 0) {
-        node->leaves = new minmax_tree *[node->leaves_count];
+    if (depth > 0 && node.leaves_count > 0) {
 
+        char next_player = (player == 'O') ? ('X') : ('O');
         // for every leaf create nodes with updated board
-        for (int i = 0;i < node->leaves_count;i++) {
-            std::string temp_game;
-            std::memcpy(&temp_game, &act_game, sizeof act_game);
+        for (size_t i = 0;i < node.leaves_count;i++) {
+            std::string temp_game = act_game;
 
             // change board to actual state
-            do_move(temp_game, act_move_list[i]);
-            node->leaves[i] = create_tree(depth - 1, temp_game, act_move_list[i].end);
+            do_move(temp_game, act_move_list[i], player);
+            node.leaves.push_back(create_tree(depth - 1, temp_game, act_move_list[i].end, next_player));
         }
-    }
-    else {
-        node->leaves = NULL;
     }
 
     return node;
@@ -201,7 +216,8 @@ minmax_tree *create_tree(int depth, std::string act_game, int act_tile) {
 
 // heuristic functions to calculate value of movement
 void calculate_parity_stability(std::string act_game, double& parity, double& empty_spot, double& disk_squares) {
-    int my_coins = 0, opp_coins = 0, my_neighbour_empty = 0, opp_neighbour_empty = 0;
+    size_t my_coins = 0, opp_coins = 0, my_neighbour_empty = 0, opp_neighbour_empty = 0;
+    disk_squares = 0;
 
     std::vector<int> neighbours{ -SIZE - 1, -SIZE, -SIZE + 1 , -1, +1, +SIZE - 1, +SIZE , +SIZE + 1 };
 
@@ -227,8 +243,8 @@ void calculate_parity_stability(std::string act_game, double& parity, double& em
             }
             // calculates empty spots around tile
             for (int j = 0; j < SIZE; j++) {
-                int neighbour = i + neighbours[i];
-                if (!out_of_board(i, neighbour) && act_game[neighbour] == ' ') {
+                int neighbour = i + neighbours[j];
+                if (!out_of_board(i, neighbours[j]) && act_game[neighbour] == '-') {
                     if (act_game[i] == my_player.character) {
                         my_neighbour_empty++;
                     }
@@ -281,19 +297,18 @@ void calculate_mobility(std::string act_game, double& mobility) {
 }
 
 
-double calculate_corners(std::string act_game, double& corners, double& close_corners) {
-    int my_corners = 0, opp_corners = 0;
-    int my_close_corners = 0, opp_close_corners = 0;
+void calculate_corners(std::string act_game, double& corners, double& close_corners) {
+    int my_corners = 0, opp_corners = 0, my_close_corners = 0, opp_close_corners = 0;
     char opp = (my_player.character == 'O') ? ('X') : ('O');
 
-    std::vector<int> corners_coor = { 0, SIZE - 1, SIZE * (SIZE - 1), SIZE * SIZE - 1 };
-    std::vector<int> close_corners_coor = { 1, SIZE + 1, SIZE,
-                                        SIZE - 2, 2 * SIZE - 2, 2 * SIZE - 1,
-                                        (SIZE - 2) * SIZE, (SIZE - 2) * SIZE + 1, (SIZE - 1) * SIZE + 1,
-                                        (SIZE - 1) * SIZE - 1, (SIZE - 1) * SIZE - 2, SIZE * SIZE - 2
-    };
+    std::vector<size_t> corners_coor = { 0, SIZE - 1, SIZE * (SIZE - 1), SIZE * SIZE - 1 };
+    std::vector<size_t> close_corners_coor = {  1, SIZE + 1, SIZE,
+                                                SIZE - 2, 2 * SIZE - 2, 2 * SIZE - 1,
+                                                (SIZE - 2) * SIZE, (SIZE - 2) * SIZE + 1, (SIZE - 1) * SIZE + 1,
+                                                (SIZE - 1) * SIZE - 1, (SIZE - 1) * SIZE - 2, SIZE * SIZE - 2
+                                             };
     
-    for (int i = 0;i < corners_coor.size();i++) {
+    for (size_t i = 0;i < corners_coor.size();i++) {
         if (act_game[corners_coor[i]] == my_player.character) {
             my_corners++;
         }
@@ -301,7 +316,7 @@ double calculate_corners(std::string act_game, double& corners, double& close_co
             opp_corners++;
         }
         else {
-            for (int j = 0;j < 3;j++) {
+            for (size_t j = 0;j < 3;j++) {
                 if (act_game[close_corners_coor[i * 3 + j]] == my_player.character) {
                     my_close_corners++;
                 }
@@ -312,8 +327,8 @@ double calculate_corners(std::string act_game, double& corners, double& close_co
         }
     }
 
-    corners = 25 * (my_corners - opp_corners);
-    close_corners = - 12.5 * (my_close_corners - opp_close_corners);
+    corners = 25. * (my_corners - opp_corners);
+    close_corners = -12.5 * (my_close_corners - opp_close_corners);
 }
 
 
@@ -331,22 +346,23 @@ double heuristic(std::string act_game) {
     calculate_corners(act_game, corners, close_corners);
 
     // FINAL SCORE - adding different weights to different evaluations (from source)
+    // std::cout << "PARITY " << parity << std::endl << "CORNERS " << corners << std::endl << "CLOSE CORNERS " << close_corners << std::endl << "MOBILITY " << mobility << std::endl << "EMPTY SPOTS " << empty_spots << std::endl << "DISK SQUARES " << disk_squares << std::endl << "-----" << std::endl;
     return (10 * parity) + (801.724 * corners) + (382.026 * close_corners) + (78.922 * mobility) + (74.396 * empty_spots) + (10 * disk_squares);
 }
 
 
 // AI program which returns optimal value for current player
-double minimax(int depth, minmax_tree *node, bool maximizing, double alpha, double beta) {
-    if (depth == 0) { //TU TREBA DODAT ZE KED BUDE GAME OVER
-        return heuristic(node->game);
+double minmax(int depth, minmax_tree& node, bool maximizing, double alpha, double beta) {
+    if (depth == 0 || game_over(node.game)) {
+        return heuristic(node.game);
     }
 
     if (maximizing) {
         double max_val = -DBL_MAX;
 
         // recur for every leaf
-        for (int i = 0; i < node->leaves_count; i++) {
-            double eval = minimax(depth - 1, node->leaves[i], false, alpha, beta);
+        for (size_t i = 0; i < node.leaves_count; i++) {
+            double eval = minmax(depth - 1, node.leaves[i], false, alpha, beta);
             max_val = std::max(max_val, eval);
 
             // alpha beta pruning
@@ -355,7 +371,7 @@ double minimax(int depth, minmax_tree *node, bool maximizing, double alpha, doub
                 break;
             }
         }
-        node->value = max_val;
+        node.value = max_val;
         return max_val;
     }
     else {
@@ -363,7 +379,7 @@ double minimax(int depth, minmax_tree *node, bool maximizing, double alpha, doub
 
         // recur for every leaf
         for (int i = 0; i < 2; i++) {
-            double eval = minimax(depth - 1, node->leaves[i], true, alpha, beta);
+            double eval = minmax(depth - 1, node.leaves[i], true, alpha, beta);
             min_val = std::min(min_val, eval);
             beta = std::min(beta, min_val);
 
@@ -372,16 +388,17 @@ double minimax(int depth, minmax_tree *node, bool maximizing, double alpha, doub
                 break;
             }
         }
-        node->value = min_val;
+        node.value = min_val;
         return min_val;
     }
 }
 
 
-void find_tile(int tile) {
-    int row = tile / SIZE;
-    int col_num = tile % SIZE;
-    char col;
+// helper function to answer with tile
+void find_tile(size_t tile) {
+    size_t row = tile / SIZE;
+    size_t col_num = tile % SIZE;
+    char col = '-';
 
     switch (col_num) {
     case 0:
@@ -410,7 +427,7 @@ void find_tile(int tile) {
         break;
     }
 
-    std::cout << row + 1 << col << std::endl;
+    std::cout << col << row + 1 << std::endl;
 }
 
 
@@ -429,12 +446,14 @@ int main(){
                 check_length(parameters, 3);
                 check_color(parameters[1]);
                 check_number(parameters[2]);
+
                 // assigning
                 my_player.color = parameters[1][0];   // string to char
                 (my_player.color == 'W') ? (my_player.character = 'O') : (my_player.character = 'X');
                 my_player.time = stoi(parameters[2]);
                 assignedStart = true;
                 std::cout << "1" << std::endl;
+                //std::cout << "MOVE ---------------------------OX------XO---------------------------";
             }
             else if (parameters[0] == "STOP") {
                 // validation
@@ -443,24 +462,36 @@ int main(){
             }
             else if (parameters[0] == "MOVE") {
                 if (assignedStart) {
+
                     // validation
                     check_length(parameters, 2);
                     check_game(parameters[1]);
                     std::string act_game = parameters[1];
+
                     // for every turn create game tree
-                    minmax_tree *tree = create_tree(MINMAX_DEPTH, act_game, -1);
+                    minmax_tree tree = create_tree(MINMAX_DEPTH, act_game, SIZE*SIZE, my_player.character);  // bcs SIZE^2 is out of board
                     bool maximizer = (my_player.color == 'B') ? true : false;
+
                     // run minmax algorithm
-                    double return_val = minimax(MINMAX_DEPTH, tree, -DBL_MAX, DBL_MAX, maximizer);
+                    double return_val = minmax(MINMAX_DEPTH, tree, maximizer, -DBL_MAX, DBL_MAX);
+
                     // traverse through first layer of tree to find node with optimal value
-                    for (int i = 0;i < tree->leaves_count;i++) {
-                        if (tree->leaves[i]->value == return_val) {
+                    for (size_t i = 0;i < tree.leaves_count;i++) {
+                        if (tree.leaves[i].value == return_val) {
+
                             // find tile and answer
-                            find_tile(tree->leaves[i]->tile);
+                            std::cout << "-----------" << std::endl;
+                            find_tile(tree.leaves[i].tile);
                             break;
                         }
                     }
-                    // TREBA DOIMPLEMENTOVAT CAS
+
+                    parameters.clear();
+                    // TREBA DOIMPLEMENTOVAT CAS + KED RAC NEMA TAH DAVA PASS
+
+                    // TU BUDE ODPOVED POCITACA PRE OTESTOVANIE
+                    std::string pc_game;
+                    std::cout << "PCs turn" << std::endl;
                 }
                 else {
                     throw_error("START parameters not assigned.");
